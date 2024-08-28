5cb5c4244c0e27d55f7b4aa44fae1a63b634f7c4
Paul156551 <pavel.ryzhkov.99@mail.ru>
2024-08-28 13:51:06 +0300

    Commit Changes

--- src/main/java/ru/akutepov/spring/cache/example/model/Book.java
+++ src/main/java/ru/akutepov/spring/cache/example/model/Book.java
@@ -1,9 +1,9 @@
 package ru.akutepov.spring.cache.example.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "book")
