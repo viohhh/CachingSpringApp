8f59455f73191d2534d357b56f1f0f6ed0da0fee
Paul156551 <pavel.ryzhkov.99@mail.ru>
2024-08-28 13:47:51 +0300

    Commit Changes

--- src/main/java/ru/akutepov/spring/cache/example/configuration/AppConfiguration.java
+++ src/main/java/ru/akutepov/spring/cache/example/configuration/AppConfiguration.java
@@ -1,6 +1,5 @@
 package ru.akutepov.spring.cache.example.configuration;
 
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
